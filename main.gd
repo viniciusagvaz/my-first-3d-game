2d0709ae4a4eb134ba8cbe4f49cecbbe80b5e3f9
viniciusagvaz <agvazvinicius@gmail.com>
2023-09-29 16:47:02 -0300

    improvised walls

--- main.gd
+++ main.gd
@@ -2,26 +2,22 @@ extends Node
 
 @export var mob_scene: PackedScene
 
-#Let's code the mob spawning logic. We're going to:
-#1.Instantiate the mob scene.
-#2.Sample a random position on the spawn path.
-#3.Get the player's position.
-#4.Call the mob's initialize() method, passing it the random position and the player's position.
-#5.Add the mob as a child of the Main node.
-	
+
 func _on_mob_timer_timeout():
-	# Create a new instance of the Mob Scene
+	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
-	
-	# Choose a random location on the SpawnPath
-	# We store the reference to the SpawnLocation node
+
+	# Choose a random location on the SpawnPath.
+	# We store the reference to the SpawnLocation node.
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	
-	# And give it a random offset
+	# And give it a random offset.
 	mob_spawn_location.progress_ratio = randf()
-	
+
 	var player_position = $Player.position
 	mob.initialize(mob_spawn_location.position, player_position)
-	
-	# Spawn the mob by adding it to the Main Scene
+
+	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+
+func _on_player_hit():
+	$MobTimer.stop()
--- mob.gd
+++ mob.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
-# Emitted when the player jumped on the mob.
-signal squashed
-# Minimum speed of the mob in meters per second
-@export var min_speed = 10
 
-# Maximum speed of the mob in meters per second
+# Minimum speed of the mob in meters per second.
+@export var min_speed = 10
+# Maximum speed of the mob in meters per second.
 @export var max_speed = 18
+
+# Emitted when the player jumped on the mob
+signal squashed
 
 func _physics_process(_delta):
 	move_and_slide()
@@ -18,6 +19,7 @@ func initialize(start_position, player_position):
 	# Rotate this mob randomly within range of -90 and +90 degrees,
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
+
 	# We calculate a random speed (integer)
 	var random_speed = randi_range(min_speed, max_speed)
 	# We calculate a forward velocity that represents the speed.
@@ -26,11 +28,9 @@ func initialize(start_position, player_position):
 	# in order to move in the direction the mob is looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
-
-
-func _on_visibility_notifier_screen_exited():
+func _on_visible_on_screen_notifier_3d_screen_exited():
 	queue_free()
 
 func squash():
 	squashed.emit()
-	queue_free()
+	queue_free() # Destroy this node
